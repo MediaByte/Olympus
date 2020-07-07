039295e628d76ae6ea74491d6870658a60b9c65b
Mario Martin <mediabytes@icloud.com>
2020-07-07 14:29:09 -0400

    Update olympus.py

--- olympus.py
+++ olympus.py
@@ -2,7 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 """
-  Copyright © 2019 OXYS
+  Copyright © 2019
  All rights reserved.
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
